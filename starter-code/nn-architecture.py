1247431fc99b4b44f1c498814b3a2d0981ce7810
Justin X <justin.l.xiang@gmail.com>
2023-11-22 15:52:59 -0500

    documented

--- starter-code/nn-architecture.py
+++ starter-code/nn-architecture.py
@@ -18,60 +18,83 @@ class Network(object):
 
         # TODO: Implement a way to feed forward a single data point x
         # Hint: Consider outputting more than just the final approximation y_tilde
-
         # Perhaps some values in the middle of the network will be useful to compute 
         # things in backprop().
+        """
+        Taking in the 1x3 datapoint and returning loss, y_tilde, x1, z1, x2, z2
+        corresponding to the variable names in the onboarding Neural Network pdf
+        """
+
         y = x[2]
-        x1 = np.array([x[0], x[1], 1]) 
+        x1 = np.array([x[0], x[1], 1]) #1x3 array
         # print(x1)
+
         z1 = np.matmul(self.weights1, x1)
-        z1 = np.append(z1, [1], axis=0)
+        z1 = np.append(z1, [1], axis=0) #1x4 array
         # print(z1)
-        x2 = Network.relu(z1)
+
+        x2 = Network.relu(z1) #1x4 array
         # print(x2)
-        z2 = np.matmul(self.weights2, x2)
+
+        z2 = np.matmul(self.weights2, x2) #1x1 array
         # print(z2)
-        y_tilde = Network.sigmoid(z2[0])
+
+        y_tilde = Network.sigmoid(z2[0]) #scalar
         # print(y_tilde)
-        loss = Network.MSELoss(y_tilde, y)
+
+        loss = Network.MSELoss(y_tilde, y) #scalar
         # print(loss)
 
         return loss, y_tilde, x1, z1, x2, z2
     
     def backprop(self, x, y, loss, y_tilde, x1, z1, x2, z2, lRate):
-
         # TODO: Implement back propagation. 
         # Hint: Consider taking in more than just the data point x and its 
         # label y. Again, using values computed in the middle of the feedforward() method
         # will save you repeated computations.
+        """
+        Taking in datapoint, everything computed in feedforward(), and a learning rate
+        """
+
+        """
+        Computing change in loss with respect to 2nd weight layer
+        """
         dL_yt = 2 * (y_tilde-y)
         dyt_z2 = Network.d_sigmoid(z2[0])
-        dz2_W2 = np.transpose([x2])
+        dz2_W2 = np.transpose([x2]) # 4x1 vector
         # print(dL_yt,dyt_z2,dz2_W2)
-        dL_W2 = dL_yt * dyt_z2 * dz2_W2
+        dL_W2 = dL_yt * dyt_z2 * dz2_W2 # 4x1 vector
         # print(dL_W2)
-        
         for i in range(4):
             self.weights2[0][i] -= (lRate * dL_W2[i][0])
         # print(self.weights2)
 
-        dz2_x2 = np.copy(self.weights2)
-        d_relu_z1 = Network.d_relu(z1)
+        """
+        Computing change in loss with respect to 1st weight layer
+        """
+        dz2_x2 = np.copy(self.weights2) #1x4 copy of weights2
+        d_relu_z1 = Network.d_relu(z1) #1x4 derivative of relu of z1
         dx2_z1 = np.zeros((4,4))
         for i in range(4):
             dx2_z1[i][i] = d_relu_z1[i]
 
-        cutoff = dx2_z1[:,0:3]
+        """cutoff is the first 3 columns of dx2_z1 which matches the last equation on page 22"""
+        cutoff = dx2_z1[:,0:3] # 4x3 matrix
         # print(cutoff)
 
+        # X is a 3x9 matrix
         X = np.array([[x1[0], x1[1], x1[2],0,0,0,0,0,0], [0,0,0,x1[0], x1[1], x1[2],0,0,0], [0,0,0,0,0,0,x1[0], x1[1], x1[2]]])
         
+        """
+        Comoputing final result by multiplying the components then updating weights1
+        """
         matrix1 = dL_yt * dyt_z2 * dz2_x2
         # print(matrix1)
         result = np.matmul(matrix1, cutoff)
-        result = np.matmul(result, X)
+        result = np.matmul(result, X) # result is a 1x9 matrix
         # print(result)
 
+        #updating weights1
         for i in range(3):
             for j in range(3):
                 self.weights1[i][j] -= (lRate * result[0][i*3+j])
@@ -80,13 +103,14 @@ class Network(object):
 
 
     def train(self):
-
         # TODO: Implement the training algorithm, using feedforward() and backprop()
 
-        for epoch in range(100):
+        for epoch in range(1):
             for i in range(len(self.data.dataset)):
                 dataPoint = self.data.dataset[i]
                 x = [dataPoint[0], dataPoint[1], self.data.labels[i]]
+
+                """Feeding forward and then backproping each datapoint one by one for each epoch"""
                 ff = self.feedforward(x)
                 self.backprop(x[0:2], x[2], ff[0], ff[1], ff[2], ff[3], ff[4], ff[5], 0.05)
                 # print(network.weights1)
@@ -94,20 +118,17 @@ class Network(object):
 
     @staticmethod
     def sigmoid(x):
-
-        # TODO: Set sigmoid activation function 
+        # Takes in single scalar and outputs it's sigmoid
         return 1/(1+math.exp(-x))
     
     @staticmethod
     def d_sigmoid(x):
-
-        # TODO: Set derivative of sigmoid activation function 
+        #Takes in a single scalar and outputs the derivative of it's sigmoid
         return Network.sigmoid(x)*(1-Network.sigmoid(x))
     
     @staticmethod
     def relu(x):
-
-        # TODO: Set ReLU activation function 
+        # Takes in a 1x4 vector and outputs a 1x4 vector
         res = np.zeros((4))
         for i in range(0, len(x)):
             value = x[i] if x[i] > 0 else 0
@@ -116,8 +137,7 @@ class Network(object):
     
     @staticmethod
     def d_relu(x):
-
-        # TODO: Set derivative of ReLU activation function 
+        # Takes in a 1x4 vector and outputs a 1x4 vector        
         res = np.zeros((4))
         for i in range(0,len(x)):
             if x[i] > 0:
@@ -129,19 +149,17 @@ class Network(object):
     
     @staticmethod
     def MSELoss(y_tilde, y):
-
-        # TODO: Set mean-squared error cost function 
+        # Returns scalar value
         return (y-y_tilde)**2 
     
     @staticmethod
     def d_MSELoss(y_tilde, y):
-
-        # TODO: Set derivative of mean-squared error cost function wrt y_tilde
+        # Returns scalar value
         return -2*(y-y_tilde)      
     
 
 if __name__ == "__main__":
-    network = Network(1000)
+    network = Network(100000)
     # x = [network.data.dataset[0][0], network.data.dataset[0][1], network.data.labels[0]]
     # print(x)
     # x = [0.93246305, 1.00330477, 0]
@@ -152,11 +170,31 @@ if __name__ == "__main__":
 
     network.train()
 
-    #testing points
+    # Testing model on sample points
     for i in range(len(network.data.dataset)):
         dataPoint = network.data.dataset[i]
         x = [dataPoint[0], dataPoint[1], network.data.labels[i]]
         feedforward = network.feedforward(x)
         print("Loss " + str(feedforward[0]))
         print(x, "Prediction: " + str(feedforward[1]))
+
+    # # testing model on actual XOR inputs
+    # x = [0,0,0]
+    # feedforward = network.feedforward(x)
+    # print("Loss " + str(feedforward[0]))
+    # print(x, "Prediction: " + str(feedforward[1]))
+
+    # x = [1,1,0]
+    # feedforward = network.feedforward(x)
+    # print("Loss " + str(feedforward[0]))
+    # print(x, "Prediction: " + str(feedforward[1]))
+
+    # x = [1,0,1]
+    # feedforward = network.feedforward(x)
+    # print("Loss " + str(feedforward[0]))
+    # print(x, "Prediction: " + str(feedforward[1]))
+    # x = [0,1,1]
+    # feedforward = network.feedforward(x)
+    # print("Loss " + str(feedforward[0]))
+    # print(x, "Prediction: " + str(feedforward[1]))
 
